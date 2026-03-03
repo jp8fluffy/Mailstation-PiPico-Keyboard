8c2cb2de62e9d9b30b9f959e88e981755e33ffe6
fangorshile <jp8shields@gmail.com>
2026-03-03 18:34:08 -0500

    Added Comments to Columns and Rows

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ columns = [
 keyboard = KMKKeyboard()
 
 keyboard.col_pins = (
-    board.GP17,
-    board.GP2,
-    board.GP4,
-    board.GP6,
-    board.GP8,
-    board.GP11,
-    board.GP13,
-    board.GP15
+    board.GP17, # 0
+    board.GP2, # 1
+    board.GP4, # 2
+    board.GP6, # 3
+    board.GP8, # 4
+    board.GP11, # 5
+    board.GP13, # 6
+    board.GP15 # 7
 )
  
 keyboard.row_pins = (
-    board.GP0,
-    board.GP9,
-    board.GP1,
-    board.GP12,
-    board.GP3,
-    board.GP14,
-    board.GP5,
-    board.GP16,
-    board.GP7,
-    board.GP10
+    board.GP0, # 0
+    board.GP9, # 1
+    board.GP1, # 2
+    board.GP12, # 3
+    board.GP3, # 4
+    board.GP14, # 5
+    board.GP5, # 6
+    board.GP16, # 7
+    board.GP7, # 8
+    board.GP10 # 9
 )
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
@@ -45,11 +45,21 @@ keyboard.keymap = [
      KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U,
      KC.I, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, KC.SCOLON, KC.QUOTE, KC.ENTER,
      KC.CAPSLOCK, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J,
-     KC.K, KC.L, KC.KP_COMMA, KC.DOT, KC.SLASH, KC.UP, KC.DOWN, KC.RIGHT,
+     KC.K, KC.L, KC.COMMA, KC.DOT, KC.SLASH, KC.UP, KC.DOWN, KC.RIGHT,
      KC.LSHIFT, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M,
-     KC.LGUI, KC.NO, KC.NO, KC.SPACE, KC.NO, KC.NO, KC.RSHIFT,
+     KC.RCTRL, KC.NO, KC.NO, KC.SPACE, KC.NO, KC.NO, KC.RSHIFT, KC.NO
      ]
 ]
+# keyboard.keymap = [
+#         [
+#         KC.DELETE, KC.BSPACE, KC.PSCREEN, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.N2, KC.NO, KC.NO,
+#         KC.GRAVE, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINUS, KC.EQUAL, KC.BSPACE, KC.PGUP,
+#         KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, KC.BSLASH, KC.BSLASH, KC.PGDN,
+#         KC.CAPSLOCK, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.SCOLON, KC.QUOTE, KC.ENTER, 
+#         KC.LSHIFT, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH, KC.RSHIFT, KC.UP,
+#         KC.LGUI, KC.NO, KC.NO, KC.SPACE, KC.NO, KC.NO, KC.LEFT, KC.RIGHT, KC.DOWN
+#         ]
+# ]
 
 if __name__ == '__main__':
     keyboard.go()
